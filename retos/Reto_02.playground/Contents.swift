b33bc05830ddf37b01f290774484ccde3bcd9a8e
RodilNttData <rpampana@emeal.nttdata.com>
2022-06-20 17:27:28 -0500

    improve code
    --

--- retos/Reto_02.playground/Contents.swift
+++ retos/Reto_02.playground/Contents.swift
@@ -10,7 +10,7 @@ let DATE_FORMAT     = "dd/MM/yyyy"
 
 
 extension StringProtocol {
-    var firstUppercased: String { return self.prefix(1).uppercased() + dropFirst() }
+    var firstUppercased: String { return self.capitalized }
 }
 
 
@@ -24,7 +24,7 @@ struct UserModel {
     let sex:String
     let email:String
     let qtyBrothers:Int
-    var user:String
+    var user:String?=nil
     
     init(name:String, fatherSurname:String, motherSurname:String, dni:Int, birthDate:String, sex:String, email:String, qtyBrothers:Int) {
         self.name = name
@@ -35,7 +35,6 @@ struct UserModel {
         self.sex = sex
         self.email = email
         self.qtyBrothers = qtyBrothers
-        self.user = ""
     }
     
     func getShortName() -> String {
@@ -77,73 +76,19 @@ struct UserModel {
 
 /**User Controller**/
 class UserController {
-    var userList = Array<UserModel>()
+    var userList = [UserModel]()
     
     init(){
         initUserList()
     }
     
     func initUserList(){
-        let user1 = UserModel( name:"CARLOS JOSÉ",
-                               fatherSurname:"ROBLES",
-                               motherSurname:"GOMES",
-                               dni: 78451245,
-                               birthDate:"06/08/1995",
-                               sex:"M",
-                               email:"carlos.roblesg@hotmail.com",
-                               qtyBrothers:2)
-        
-        let user2 = UserModel( name:"MIGUEL ANGEL",
-                               fatherSurname:"QUISPE",
-                               motherSurname:"OTERO",
-                               dni: 79451654,
-                               birthDate:"28/12/1995",
-                               sex:"M",
-                               email:"miguel.anguel@gmail.com",
-                               qtyBrothers:0)
-        
-        let user3 = UserModel( name:"KARLA ALEXANDRA",
-                               fatherSurname:"FLORES",
-                               motherSurname:"ROSAS",
-                               dni: 77485812,
-                               birthDate:"15/02/1997",
-                               sex:"F",
-                               email:"Karla.alexandra@hotmail.com",
-                               qtyBrothers:1)
-        
-        let user4 = UserModel( name:"NICOLAS",
-                               fatherSurname:"QUISPE",
-                               motherSurname:"ZEBALLOS",
-                               dni: 71748552,
-                               birthDate:"08/10/1990",
-                               sex:"M",
-                               email:"nicolas123@gmail.com",
-                               qtyBrothers:1)
-        
-        let user5 = UserModel( name:"PEDRO ANDRE",
-                               fatherSurname:"PICASSO",
-                               motherSurname:"BETANCUR",
-                               dni: 74823157,
-                               birthDate:"17/05/1994",
-                               sex:"M",
-                               email:"pedroandrepicasso@gmail.com",
-                               qtyBrothers:2)
-        
-        let user6 = UserModel( name:"FABIOLA MARIA",
-                               fatherSurname:"PALACIO",
-                               motherSurname:"VEGA",
-                               dni: 76758254,
-                               birthDate:"02/02/1992",
-                               sex:"F",
-                               email:"fabi@hotmail.com",
-                               qtyBrothers:0)
-        
-        userList.append(user1)
-        userList.append(user2)
-        userList.append(user3)
-        userList.append(user4)
-        userList.append(user5)
-        userList.append(user6)
+        userList.append(UserModel( name:"CARLOS JOSÉ",fatherSurname:"ROBLES", motherSurname:"GOMES",dni: 78451245,birthDate:"06/08/1995",sex:"M",email:"carlos.roblesg@hotmail.com",qtyBrothers:2))
+        userList.append(UserModel( name:"MIGUEL ANGEL",fatherSurname:"QUISPE",motherSurname:"OTERO",dni: 79451654,birthDate:"28/12/1995",sex:"M",email:"miguel.anguel@gmail.com",qtyBrothers:0))
+        userList.append(UserModel( name:"KARLA ALEXANDRA",fatherSurname:"FLORES", motherSurname:"ROSAS", dni: 77485812,birthDate:"15/02/1997", sex:"F", email:"Karla.alexandra@hotmail.com",qtyBrothers:1))
+        userList.append(UserModel( name:"NICOLAS",fatherSurname:"QUISPE", motherSurname:"ZEBALLOS",dni: 71748552,birthDate:"08/10/1990",sex:"M",email:"nicolas123@gmail.com",qtyBrothers:1))
+        userList.append(UserModel( name:"PEDRO ANDRE",fatherSurname:"PICASSO",motherSurname:"BETANCUR",dni: 74823157,birthDate:"17/05/1994",sex:"M",email:"pedroandrepicasso@gmail.com",qtyBrothers:2))
+        userList.append(UserModel( name:"FABIOLA MARIA",fatherSurname:"PALACIO", motherSurname:"VEGA",dni: 76758254,birthDate:"02/02/1992",sex:"F",email:"fabi@hotmail.com",qtyBrothers:0))
         addUserNames()
     }
     
@@ -165,18 +110,18 @@ class UserController {
         }
     }
     
-    func getList() -> Array<UserModel>{
+    func getList() -> [UserModel]{
         return userList
     }
     
-    func getListBySex(sex:String) -> Array<UserModel>{
+    func getListBySex(sex:String) -> [UserModel]{
         let menList = userList.filter({ user in
             user.sex == sex
         })
         return menList
     }
     
-    func filterListByQtyBrothers(qty:Int) -> Array<UserModel>{
+    func filterListByQtyBrothers(qty:Int) -> [UserModel]{
         let list = userList.filter({ user in
             user.qtyBrothers > qty
         })
@@ -199,13 +144,12 @@ class UserController {
 
 /**Test**/
 let userController = UserController()
-
 print(userController.toString())
 print("")
 print("***************** Lista de Usuarios *******************\n")
 for person in userController.getList() {
     let shortName = person.getShortName()
-    let userName = person.user.lowercased()
+    let userName = person.user?.lowercased() ?? ""
     print("NOMBRE :: \(shortName.firstUppercased)        USUARIO :: \(userName) ")
 }
 
