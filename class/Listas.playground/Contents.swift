7ee5664c6f66074601d5f453738f9fac78ff0d58
RodilNttData <rpampana@emeal.nttdata.com>
2022-06-20 20:00:55 -0500

    diccionarios y listas
    --

---
+++ class/Listas.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// diccionarios
+let miDiccionario : [String:String] = ["":""]
+let diccionario : Dictionary<String, String> = ["": ""]
+
+// Arrays
+let miArray : [Int] = [1,2]
+let array : Array<Int> = [1,2]
+
+//Set
